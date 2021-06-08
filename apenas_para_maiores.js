d3568be79cb19d1a516f30c327101107992fcf46
rsvfrebeca <rebecaferreiraprofissional@gmail.com>
2021-06-08 20:18:03 -0300

    Verificar se é maior de idade e determinar acesso

---
+++ apenas_para_maiores.js
@@ -0,0 +1,9 @@
+function solucao(idade) {
+    //seu codigo aqui
+      if (idade >= 18){
+          console.log("PODE ENTRAR")
+      } else {
+          console.log("ACESSO NEGADO")
+      }
+    
+  }
