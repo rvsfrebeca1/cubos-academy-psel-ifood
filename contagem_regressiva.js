3a5d4235da5c3c529f33da0c7673a6c55922add2
rsvfrebeca <rebecaferreiraprofissional@gmail.com>
2021-06-08 20:43:14 -0300

    Contagem regressiva a partir do numero passado

---
+++ contagem_regressiva.js
@@ -0,0 +1,13 @@
+function solucao(numero) {
+    let i = 0
+    while (numero > numero - 1){
+        if (numero == 0){
+            console.log(0)
+            console.log("KABUM")
+            
+            break
+        }
+        console.log(numero)
+        numero--
+    }
+}
