8cb00af9bb8b69fda68a7fa0e61f7bf086701fc9
rsvfrebeca <rebecaferreiraprofissional@gmail.com>
2021-06-08 20:52:57 -0300

    Identifica se terá olimpiada ou copa do mundo

---
+++ tem_jogo.js
@@ -0,0 +1,9 @@
+function solucao(ano) {
+    if (ano % 4 == 2){
+        console.log("COPA")
+    } else if (ano % 4 == 0){
+        console.log("JOGOS")
+    } else{
+        console.log("MEH")
+    }
+}
